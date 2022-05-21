d1150f47e84dc4ddc0fda5d16bc1ab837b551b05
wforguo <wforguo@qq.com>
2022-05-21 20:30:31 +0800

    feat: 添加husky
    husky校验commit message

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,16 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+/**
+ * feature：新功能
+ * update：更新某功能
+ * fixbug：修补某功能的bug
+ * refactor：重构某个功能
+ * optimize: 优化构建工具或运行时性能
+ * style：仅样式改动
+ * docs：仅文档新增/改动
+ * chore：构建过程或辅助工具的变动
+ */
+module.exports = {
+    extends: [
+        // 直接继承官网规则
+        '@commitlint/config-conventional'
+    ]
+}
