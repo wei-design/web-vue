3c1519c99dfd19dbd112a5a06d1a3e4c99851637
wforguo <wforguo@qq.com>
2022-08-14 23:32:38 +0800

    feat: 修改favicon

--- docs/.vitepress/config/head.ts
+++ docs/.vitepress/config/head.ts
@@ -9,7 +9,7 @@ export const head: HeadConfig[] = [
         'link',
         {
             rel: 'icon',
-            href: 'images/logo-link.png',
+            href: '//wforguo.github.io/we-design/images/logo-link.png',
             type: 'image',
         },
     ],
