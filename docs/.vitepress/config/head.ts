ec38b8863262acae752849a1c0b41d4a6b86745f
wei <wforguo@qq.com>
2025-11-29 20:50:27 +0800

    fix(docs): 更新百度统计脚本的追踪ID并移除验证meta标签

--- docs/.vitepress/config/head.ts
+++ docs/.vitepress/config/head.ts
@@ -24,12 +24,11 @@ export const head: HeadConfig[] = [
     ['meta', {name: 'keywords', content: `@wei-design/web-vue,A Vue.js 3 UI library,vue,vue3`}],
     ['meta', {name: 'description', content: `wei-design|@wei-design/web-vue|A Vue.js 3 UI library|vue|vue3`}],
     ['meta', {name: 'author', content: `wei,wei-design`}],
-    ['meta', {name: 'baidu-site-verification', content: `code-bakUos2v8l`}],
     ['script', {}, `
         var _hmt = _hmt || [];
         (function() {
           var hm = document.createElement("script");
-          hm.src = "//hm.baidu.com/hm.js?2788f1f4f01e060d6d892f4bbd5b74d4";
+          hm.src = "//hm.baidu.com/hm.js?e2b1313ceb93a9a3428404181b004fc1";
           var s = document.getElementsByTagName("script")[0];
           s.parentNode.insertBefore(hm, s);
         })();
